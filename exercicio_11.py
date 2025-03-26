b9f432206a68e8df17d8c171d890acb9fa8b1ce1
João Monteiro <joaopb187@hotmail.com>
2025-03-26 20:55:37 -0300

    Atualização

--- exercicio_11.py
+++ exercicio_11.py
@@ -1,1 +1,9 @@
-quant_atual = int(input("Digite a quantidade atual em estoque"))
+quant_maxima = 1000
+quant_min = 800
+quant_media = (quant_maxima + quant_min) / 2
+quant_atual = int(input("Digite a quantidade atual em estoque "))
+print(f"A média é {quant_media}")
+if quant_atual >= quant_media:
+    print("Não efetuar compra")
+else : 
+    print("Efetuar compra")
